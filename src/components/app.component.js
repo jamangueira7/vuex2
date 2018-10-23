72f93d4bee0c4a9f021bfbe82fd0e83fc5b38e63
jamangueira7 <jamjs7@gmail.com>
2018-10-23 10:03:56 -0200

    Primeiro commit, projeto antes de usar o vuex

---
+++ src/components/app.component.js
@@ -0,0 +1,42 @@
+import {Time} from "../time";
+import TimeListComponent from './time-list.component';
+import TimeJogoComponent from './time-jogo.component';
+import event from '../event';
+
+export default {
+  components: {
+    'time-list':TimeListComponent,
+    'time-jogo':TimeJogoComponent,
+  },
+  template:` 
+  <div class="container">
+        <h3> Campeonato Brasileiro - Série A</h3>
+        
+        <div v-show="view == 'tabela'">
+          <time-list></time-list>
+        </div>
+    
+        <div v-show="view == 'novojogo'">
+         <time-jogo></time-jogo>
+        </div>
+  </div>`,
+  mounted() {
+    event.$on('show-time-list',()=> {
+        this.view = 'tabela';
+    });
+    event.$on('show-time-novojogo',()=> {
+      this.view = 'novojogo';
+    });
+  },
+  data() {
+    return {
+
+      view: 'tabela'
+    };
+  },
+  methods: {
+    showView(view){
+      this.view = view;
+    }
+  }
+}
